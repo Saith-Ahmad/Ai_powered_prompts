2d4b565a128e9e8e3e3ea904314839808514400f
Saith_Ahmad <ahmadwebcrafts@gmail.com>
2024-07-02 18:49:40 +0500

    correct prompt spellings

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,7 +22,7 @@ export const Nav = () => {
     <nav className="flex-between w-full mb-16 p-3">
       <Link href={'/'} className="flex gap-2 flex-center">
         <Image src={'/assets/images/logo.svg'} alt="logo" width={30} height={30} className="object-contain" />
-        <p className="logo_text">Promtopia</p>
+        <p className="logo_text">Promptopia</p>
       </Link>
 
 
